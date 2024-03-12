c173049e278c2585f10dec9415237ae25e6bca3a
travel15740 <travel15740@gmail.com>
2024-03-12 16:34:53 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router()
 
 const admin = require('./modules/admin')
 
-const restController = require('../controllers/restaurant-controller') // 載入 controller
+// 載入 controller
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))

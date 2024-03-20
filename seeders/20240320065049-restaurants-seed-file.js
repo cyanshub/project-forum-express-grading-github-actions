bb72e953cd31a6d853c4c1102061a3e359e4dd3e
travel15740 <travel15740@gmail.com>
2024-03-20 15:55:11 +0800

    feat: update seed files

--- seeders/20240320065049-restaurants-seed-file.js
+++ seeders/20240320065049-restaurants-seed-file.js
@@ -2,6 +2,10 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query(
+      'SELECT `id` FROM `Categories`;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Restaurants',
       // Array.from 在一個參數設定 length 指定陣列的長度
       Array.from({ length: 50 }, () => ({
@@ -12,7 +16,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       })), {})
   },
 

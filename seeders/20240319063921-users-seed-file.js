f17d049175f622c101f0555e93cfecce88fb1fea
travel15740 <travel15740@gmail.com>
2024-03-20 09:37:07 +0800

    微調使用者密碼, 與教案一致

--- seeders/20240319063921-users-seed-file.js
+++ seeders/20240319063921-users-seed-file.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
       email: 'root@example.com',
-      password: await bcrypt.hash('qwe', 10),
+      password: await bcrypt.hash('12345678', 10),
       is_admin: true,
       name: 'root',
       created_at: new Date(),
@@ -12,14 +12,14 @@ module.exports = {
     },
     {
       email: 'user1@example.com',
-      password: await bcrypt.hash('qwe', 10),
+      password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user1',
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user2@example.com',
-      password: await bcrypt.hash('qwe', 10),
+      password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user2',
       created_at: new Date(),

b9afa8f79486cfa7017296ba0fbf702ba340233b
travel15740 <travel15740@gmail.com>
2024-03-20 14:50:37 +0800

    feat: add category model

---
+++ models/category.js
@@ -0,0 +1,21 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    static associate (models) {
+      // define association here
+      Category.hasMany(models.Restaurant, { foreignKey: 'categoryId' })
+    }
+  };
+  Category.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Category',
+    tableName: 'Categeories',
+    underscored: true
+  })
+  return Category
+}

0fce40145696cc8ffa3e97200cf82d9872994a7a
travel15740 <travel15740@gmail.com>
2024-03-25 11:07:27 +0800

    餐廳資訊整理: Dashboard修改

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -40,21 +40,7 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      raw: true,
-      include: [Category], // 拿出關聯的 Category model
-      nest: true
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
-      })
-      .catch(err => next(err))
-  },
-  getDashboard: (req, res, next) => {
-    // res.render('restaurant-dashboard')
-    return Restaurant.findByPk(req.params.id, {
-      include: [Category],
-      nest: true
+      include: [Category] // 拿出關聯的 Category model
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
@@ -62,7 +48,20 @@ const restaurantController = {
         restaurant.increment('viewCounts', { by: 1 })
         return restaurant.toJSON()
       })
-      .then(restaurant => res.render('restaurant-dashboard', { restaurant }))
+      .then(restaurant => res.render('restaurant', { restaurant }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    // res.render('restaurant-dashboard')
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      include: [Category],
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant-dashboard', { restaurant })
+      })
       .catch(err => next(err))
   }
 }

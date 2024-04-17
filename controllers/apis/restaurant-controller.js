a9f83519caaaa75f7aa70c1a0f02df21f6188b4e
travel15740 <travel15740@gmail.com>
2024-04-17 16:52:55 +0800

    feat: add restaurant-services

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -1,48 +1,8 @@
-// 載入操作資料表所需的 Model
-const { Restaurant, Category } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9 // 預設每頁顯示幾筆資料
-    const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1 // 預設第一頁或從query string拿資料
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT // 預設每頁顯示資料數或從query string拿資料
-    const offset = getOffset(limit, page)
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        raw: true,
-        where: {
-          // 展開運算子的優先級較低, 會比較慢判斷
-          // 若 categoryId 存在, 則展開 {categoryId}; 若不存在則展開 {}
-          ...categoryId ? { categoryId } : {}
-        },
-        offset,
-        limit,
-        include: [Category],
-        nest: true,
-        order: [['id', 'DESC']]
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
-        const likedRestaurantsId = req.user?.LikedRestaurants ? req.user.LikedRestaurants.map(fr => fr.id) : []
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: favoritedRestaurantsId.includes(r.id),
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-
-        return res.json({
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
   }
 }
 

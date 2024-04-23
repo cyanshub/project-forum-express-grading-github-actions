616f7246d44607c10ffb0f6e7df7440ac7d9b362
travel15740 <travel15740@gmail.com>
2024-04-23 13:45:09 +0800

    feat: api signin add jwt token

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,34 @@
+// 呼叫取用 dotenv 設定檔
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 載入所需工具
+const jwt = require('jsonwebtoken')
+
+const userController = {
+  signIn: (req, res, next) => {
+    // 利用 try catch 處理不是非同步語法的錯誤事件
+    try {
+      // 因為關閉了session功能, 因此要自己整理 user 的 sequelize 打包物件
+      const userData = req.user.toJSON()
+
+      // 不應該拿 user 的 password, 可使用 delete 方法把敏感資料拿掉
+      delete userData.password
+
+      // 利用 jwt 來發 token
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('../../config/passport.js')
 const admin = require('./modules/admin.js')
 
 // 載入 controller
 const restController = require('../../controllers/apis/restaurant-controller')
+const userController = require('../../controllers/apis/user-controller.js')
 
 // 載入 middleware
 const { apiErrorHandler } = require('../../middleware/error-handler.js')
-router.use('/admin', admin)
 
 // 設計路由
+router.use('/admin', admin)
+
+// 登入的路由: 關閉 passport 的 session 功能
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', apiErrorHandler)
 
